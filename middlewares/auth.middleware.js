7240642bb479b6bdea9973c4f34090776c4022f2
Joe <shengzhe.zhou@gmail.com>
2025-11-18 01:06:34 +1100

    refactor(auth): update authorize middleware for httpOnly cookie support

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -6,7 +6,11 @@ const authorize = async (req, res, next) => {
     try{
         let token;
 
-        if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+        if (req.cookies && req.cookies.jwt) {
+            token = req.cookies.jwt;
+        }
+
+        else if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
             token = req.headers.authorization.split(' ')[1];
         }
 
